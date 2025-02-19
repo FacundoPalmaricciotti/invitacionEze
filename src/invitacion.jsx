7d2802ed559a8e71390b965f921db6c1be6eee1f
FacundoPalmaricciotti <palmaricciotti2024@gmail.com>
2025-02-18 23:50:23 -0300

    Resolviendo sorpresa c410000onflictos

--- src/invitacion.jsx
+++ src/invitacion.jsx
@@ -175,7 +175,7 @@ const Invitacion = () => {
     setContadorFondo((prev) => prev + 1);
   };
   useEffect(() => {
-    if (contadorFondo >= 5) {
+    if (contadorFondo >= 10000) {
       navigate("/sorpresa"); // ✅ Ahora la redirección es segura
     }
   }, [contadorFondo]); // Se ejecuta solo cuando cambia contadorFondo  
